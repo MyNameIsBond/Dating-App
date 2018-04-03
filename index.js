0f8c73cf528837bf8a9823e70b8eb734345d19e9
mition hajdini <hajdinim@uni.coventry.ac.uk>
2018-04-03 23:30:06 +0100

    this is cool

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app  = express()
+const app = express()
 
 app = express()
 
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.send('done!!')
+})
+
+
+app.get('/test', (req, res) => {
+  res.send('hello')
 })
