0079a36186a3472b98bddeee51b6b423ffb0377b
hajdinim <hajdinim@coventry.ac.uk>
2018-04-23 17:59:26 +0100

    mongodb on the way

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,1 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const UserPost = mongoose.Schema({
+  Post: String,
+  date: Date.now,
+  required: true
+})
+
+let Post = module.exports = mongoose.names()
--- public/js/home.js
+++ public/js/home.js
@@ -16,5 +16,23 @@ nav_bar()
 
 
 document.getElementById('refreshbtn').addEventListener('click', () => {
-  document.getElementById('textarea').value =''
+  document.getElementById('textarea').value = ''
 })
+
+function sendpost() {
+  post = document.getElementById('textarea').value
+  console.log(post);
+  const xml = new XMLHttpRequest()
+  xml.onload = function() {
+    if (xml.readyState === 4 && xml.status === 200) {
+      document.getElementById('nav').innerHTML = this.responseText
+      console.log(this.responseText)
+    } else {
+      const error = 'error'
+      console.log(error, this.statusText)
+    }
+  }
+
+  xml.open('POST', `/post/${post}`, true)
+  xml.send()
+}
