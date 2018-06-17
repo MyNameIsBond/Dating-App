7cc243e0739bb2d329a8bf81e1e99d85f21839bb
Mition Hajdini <hajdinim@uni.coventry.ac.uk>
2018-06-17 04:45:48 +0100

    profile done!

--- public/js/profile.js
+++ public/js/profile.js
@@ -41,23 +41,17 @@ function userInfoMenu() {
 }
 
 
-function openProfileTab(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
+function openProfileTab(evt, cityName, linkname) {
+  const tabcontent = document.getElementsByClassName('tabcontent'); //tabcontent
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
+  const tablinks = document.getElementsByClassName('tablinks'); //tablinks
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace("prof_tab_active", '');
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+  document.getElementById(cityName).style.display = "grid";
+  evt.currentTarget.className += "prof_tab_active";
 }
