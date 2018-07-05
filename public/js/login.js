8ef92399507b3d9cb21acacd9599d32ddfbcc992
mynameisbond <hajdinim@uni.coventry.ac.uk>
2018-07-05 21:33:07 +0100

    messages

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 window.document.getElementById('login_btn').addEventListener('click', () => {
-    const xml = new XMLHttpRequest()
-    xml.onload = function() {
+  const xml = new XMLHttpRequest()
+  xml.onload = function() {
     if (xml.readyState === 4 && xml.status === 200) {
       document.getElementById('profile').innerHTML = this.responseText
     } else {
@@ -8,26 +8,32 @@ window.document.getElementById('login_btn').addEventListener('click', () => {
       console.log(error, this.statusText)
     }
   }
-    xml.open('POST','/profile',true)
-    xml.send()
+  xml.open('POST', '/profile', true)
+  xml.send()
 })
 
 
 window.document.getElementById('reg_button').addEventListener('click', () => {
-    const username = document.getElementById('reg_username').value
-    const email = document.getElementById('reg_email').value
-    const password = document.getElementById('reg_password').value
-    const password2 = document.getElementById('reg_password2').value
-    const gender = document.getElementById('gender').value
-    console.log(gender, username, password2, email)
+  const username = document.getElementById('reg_username').value
+  const email = document.getElementById('reg_email').value
+  const password = document.getElementById('reg_password').value
+  const password2 = document.getElementById('reg_password2').value
+  const gender = document.getElementById('gender').value
+
+  let user = {
+    'username': username,
+    'email': email,
+    'password': password,
+    'password2': password2
+  }
 })
 
 function tabs(tab, cl) {
-    let form = document.getElementsByClassName('form')
-    for (let i = 0; i < form.length; i++) {
-        form[i].style.display = 'none'
-    }
-    document.getElementById(tab).style.display = 'block'
-    let tabs = document.getElementById(cl)
-    tabs.classList.add('actives')
+  let form = document.getElementsByClassName('form')
+  for (let i = 0; i < form.length; i++) {
+    form[i].style.display = 'none'
+  }
+  document.getElementById(tab).style.display = 'block'
+  let tabs = document.getElementById(cl)
+  tabs.classList.add('actives')
 }
--- routes/log-reg.js
+++ routes/log-reg.js
@@ -10,41 +10,65 @@ const session = require('express-session')
 
 
 router.get('/', (req, res) => {
-    console.log(req.session)
-    res.render('login')
+  console.log(req.session)
+  res.render('login')
 })
 
 router.post('/', (req, res) => {
-    console.log(req.session)
-    res.render('login')
+  console.log(req.session)
+  res.render('login')
 })
 
 router.post('/register', (req, res) => {
-    console.log('hello from the other side.')
+  console.log('hello from the other side.')
+
+  // check
+
+  req.checkBody('email', 'This is not a valid e mail').isEmail()
+  req.checkBody('username', 'Type your username Please').notEmpty().isLength({
+    min: 8
+  })
+  req.checkBody('password', 'Type a password please').isEmpty().isLength({
+    min: 8
+  })
+  req.checkBody('password2', 'Passwords Does not match').equals(req.body.password)
+
+  let errors = req.validationErrors()
+
+  if (errors) {
+    console.log(errors)
+    res.render('login', {
+      locals: {
+        errors: errors
+      }
+    })
+  } else {
     let user = new User()
     user.username = req.body.username
     user.email = req.body.email
     user.gender = req.body.gender
     user.password = req.body.password
-
     user.save(err => {
-        if (err) throw err
-        else return res.redirect('/')
+      if (err) throw err
+      else
+        req.flash('all Done')
+      return res.redirect('/')
     })
+  }
 })
 
-router.get('/logout', function (req, res, next) {
-    if (req.session) {
-        // delete session object
-        req.session.destroy(function (err) {
-            if (err) {
-                return next(err)
-            } else {
-                return res.redirect('/')
-            }
-        })
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if (err) {
+        return next(err)
+      } else {
+        return res.redirect('/')
+      }
+    })
 
-    }
+  }
 })
 
 module.exports = router
